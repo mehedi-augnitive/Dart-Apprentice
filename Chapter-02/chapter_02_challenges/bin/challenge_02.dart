1dc89de984384d8a5354486bf22f6e058066f83a
Mehedi <babu.oshi@gmail.com>
2022-04-23 20:14:25 +0600

    Chapter-2 mini exercise and challenges completed

---
+++ Chapter-02/chapter_02_challenges/bin/challenge_02.dart
@@ -0,0 +1,14 @@
+void challenge2() {
+
+  // sets an integer variable age
+  var age = 16;
+
+  // prints the age
+  print("Challenge-2: age = $age");
+
+  // assigns a new value to the variable age
+  age = 30;
+
+  // prints the new age
+  print("Challenge-2: age = $age");
+}
---
+++ Chapter-02/chapter_02_challenges/bin/challenge_04.dart
@@ -0,0 +1,18 @@
+void challenge4() {
+
+  // initializes a constant variable and assigns double value to it
+  const rating1 = 13.69;
+
+  // initializes a constant variable and assigns double value to it
+  const rating2 = 5.41;
+
+  // initializes a constant variable and assigns double value to it
+  const rating3 = 9.78;
+
+  // finds the average of 3 constant double variables which by logic is a constant
+  // and assigns the value to a constant double variable
+  const average = (rating1 + rating2 + rating3) / 3;
+
+  // prints constant double variable average
+  print("Challenge-4: average = $average");
+}
