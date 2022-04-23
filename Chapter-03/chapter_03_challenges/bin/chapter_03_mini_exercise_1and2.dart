4c462e4bf75c0d43b68e8690a7ea115a6d22aec2
Mehedi <babu.oshi@gmail.com>
2022-04-23 21:44:02 +0600

    Chapter-3 mini exercise completed

---
+++ Chapter-03/chapter_03_challenges/bin/chapter_03_mini_exercise_1and2.dart
@@ -0,0 +1,14 @@
+void miniExercise1() {
+
+  /* Initializes constant integer variable.
+  * Without any type defined explicitly const acts like var.
+  */
+  const age1 = 42;
+  const age2 = 21;
+
+  //Constant variable averageAge holds double value from the calculation.
+  const averageAge = (age1 + age2) / 2;
+
+  // prints the runtime variable type of averageAge.
+  print(averageAge.runtimeType);
+}
