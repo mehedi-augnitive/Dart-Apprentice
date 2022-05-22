a57fda3476663747a9f02c6ab0efb547874ea268
Mehedi <babu.oshi@gmail.com>
2022-05-23 04:15:06 +0600

    chapter-4, challenge-4 completed

--- Chapter-04/chapter_04_challenges_and_mini_exercises/bin/chapter_04_challenge_04.dart
+++ Chapter-04/chapter_04_challenges_and_mini_exercises/bin/chapter_04_challenge_04.dart
@@ -1,12 +1,24 @@
-void challenge4(){
-
+void challenge4() {
   // prints the challenge number
   print("\tChallenge-4:");
 
-  // Initializes the iteration number
+  // Initializes the iteration number n = 17
   const iteration = 17;
 
   var previousNumber = 1;
   var currentNumber = 1;
+  var fibonacciNumber = 0;
 
+  /*
+   *loop initialization = 2 as two Fibonacci numbers(1 and 1) are initialized
+   * as previous and current numbers
+  */
+  for (int i = 2; i < 17; i++) {
+    fibonacciNumber = previousNumber + currentNumber;
+    previousNumber = currentNumber;
+    currentNumber = fibonacciNumber;
+  }
+
+  // prints the Fibonacci Number up to nth
+  print("\tFibonacci Number up to n = $iteration is: $fibonacciNumber");
 }
