b4d58ea71e4dd54307fd31105a4d9abb3028953f
Mehedi <babu.oshi@gmail.com>
2022-05-25 01:47:27 +0600

    chapter-5 mini exercise 1 completed

---
+++ Chapter-5/chapter_05_mini_exercises_and_challenges/bin/chapter_05_mini_exercises_and_challenges.dart
@@ -0,0 +1,13 @@
+import 'chapter_05_mini_exercises/chapter_05_mini_exercise_01.dart';
+
+void main() {
+  /// prints the chapter number
+  print("Chapter-05:");
+
+  /*
+   * calls and prints youAreWonderful function
+   * with a required named String variable and
+   * a named integer variable with a String returned
+  */
+  print('\t${youAreWonderful(name: 'Bob', numberPeople: 10)}');
+}
