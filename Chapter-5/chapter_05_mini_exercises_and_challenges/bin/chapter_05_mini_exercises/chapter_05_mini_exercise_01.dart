17d6d3257a9ddb2c350d00129f020e585dd0e746
Mehedi <babu.oshi@gmail.com>
2022-05-25 01:47:27 +0600

    chapter-5 mini exercise 1 completed

---
+++ Chapter-5/chapter_05_mini_exercises_and_challenges/bin/chapter_05_mini_exercises/chapter_05_mini_exercise_01.dart
@@ -0,0 +1,13 @@
+/*
+ *youAreWonderful function
+ *with a required named String variable and a named integer variable
+ *returns a String
+*/
+String youAreWonderful({required String name, int numberPeople = 30}) {
+
+  /// prints the mini exercise number
+  print('Mini Exercise - 01:');
+
+  /// returns the desired String
+  return 'You are wonderful, $name. $numberPeople people think so.';
+}
---
+++ Chapter-5/chapter_05_mini_exercises_and_challenges/test/chapter_05_mini_exercises_and_challenges_test.dart
@@ -0,0 +1,8 @@
+import 'package:chapter_05_mini_exercises_and_challenges/chapter_05_mini_exercises_and_challenges.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
